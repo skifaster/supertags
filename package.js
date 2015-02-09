99111717ceae17ef05b1a400be5f9f143db112af
Carsten Winsnes <carsten_15@hotmail.com>
2015-02-09 08:15:31 -0800

    Restructure and adding of mongo doc tagging

--- package.js
+++ package.js
@@ -8,12 +8,17 @@ Package.describe({
 Package.onUse(function(api) { 
 	api.versionsFrom("1.0");
 	api.use("mizzao:autocomplete");
+  api.use("arsnebula:reactive-varx@0.9.1");
+
 	api.use("arsnebula:classx");
   api.use("templating");
 
 	api.addFiles([
-		"lib/supertags.namespace.js",
-		"lib/supertags.activate.js"
+		"lib/js/supertags.namespace.js",
+    "lib/js/supertags.controller.js",
+    "lib/js/supertags.mongocontroller.js",
+    "lib/js/supertags.helpers.js",
+		"lib/js/supertags.activate.js",
 		]);
 
   api.addFiles([
